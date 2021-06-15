2708b603fc197758777baac8e479542b7b879cc9
김동환 <1033513@naver.com>
2021-06-15 20:50:48 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 김동환 on 2021/06/15.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
